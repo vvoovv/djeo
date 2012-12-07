dbb8c74c3496460c55080f62fb623d338f6ff20f
Vladimir Elistratov <vvoovv.projects@gmail.com>
2012-12-07 18:23:06 +0400

    ScaleBar: further development

--- widget/ScaleBar.js
+++ widget/ScaleBar.js
@@ -1,32 +1,88 @@
 define([
 	"dojo/_base/declare", // declare
+	"dojo/_base/lang", // hitch
 	"dijit/_Widget",
 	"dijit/_TemplatedMixin",
-	"dijit/form/HorizontalRule"
-], function(declare, _Widget, _TemplatedMixin, HorizontalRule) {
+	"dijit/form/HorizontalRule",
+	"djeo/util/_base"
+], function(declare, lang, _Widget, _TemplatedMixin, HorizontalRule, u) {
 
-return declare("dijit.form.HorizontalRule", [_Widget, _TemplatedMixin], {
+var acceptableMantissas = [1, 2, 5],
+	numMantissas = acceptableMantissas.length - 1
+;
+
+return declare([_Widget, _TemplatedMixin], {
 	
-	templateString: "<div class='djeoScaleBar' style='position: absolute;'><div></div><div></div></div>",
+	minWidth: 60,
+	
+	typicalWidth: 100,
+	
+	maxWidth: 140,
+	
+	templateString: "<div class='djeoScaleBarContainer'><div class='djeoScaleBar' data-dojo-attach-point='scaleBar'><div></div><div></div></div><span class='djeoScaleBarLabel' data-dojo-attach-point='label'></span></div>",
 	
 	buildRendering: function() {
 		this.inherited(arguments);
 
 		new HorizontalRule({
-			count:6,
+			count:5,
 			style:{height:".4em"}
-		}, this.domNode.children[0]);
+		}, this.scaleBar.children[0]);
 		new HorizontalRule({
-			count:11,
+			count:9,
 			style:{height:".4em"}
-		}, this.domNode.children[1]);
+		}, this.scaleBar.children[1]);
 	},
 	
 	postCreate: function() {
 		this.inherited(arguments);
 		
+		this.calculateLength(this.map.get("zoom"), 0);
 		this.domNode.style.zIndex = 1000;
+
+		this.map.on("zoom_changed", lang.hitch(this, function(){
+			this.calculateLength(this.map.get("zoom"), 0);
+		}));
+		
 		this.map._appendDiv(this.domNode);
+	},
+	
+	calculateLength: function(zoom, lat) {
+		var earthDistance = Math.round(
+				2*Math.PI*u.earthRadius*Math.cos(u.degToRad(lat))*this.typicalWidth/(256*Math.pow(2, zoom))
+			),
+			// finding the number of digits and the first digit
+			earthDistanceStr = earthDistance.toString(),
+			numDigits = earthDistanceStr.length,
+			// rounding the mantissa
+			mantissa = earthDistance/Math.pow(10, numDigits-1)
+		;
+		for (var i=0; i<numMantissas; i++) {
+			if (acceptableMantissas[i]<=mantissa && mantissa<=acceptableMantissas[i+1]) {
+				mantissa = (mantissa - acceptableMantissas[i]) <= (acceptableMantissas[i+1] - mantissa) ?
+					acceptableMantissas[i] :
+					acceptableMantissas[i+1]
+				;
+				break;
+			}
+		}
+		if (i == numMantissas) {
+			if (mantissa - acceptableMantissas[numMantissas] < 10 - mantissa) {
+				numMantissas = acceptableMantissas[numMantissas];
+			}
+			else {
+				mantissa = 1;
+				numDigits++;
+			}
+		}
+		earthDistance = (numDigits > 3) ? Math.pow(10, numDigits-4) : Math.pow(10, numDigits-1);
+		earthDistance = Math.round(mantissa*earthDistance);
+		
+		this.label.innerHTML = earthDistance + ((numDigits > 3) ? " km" : " m");
+		if (numDigits > 3) {
+			earthDistance *= 1000;
+		}
+		this.scaleBar.style.width = Math.round(earthDistance*128*Math.pow(2, zoom)/(Math.PI*u.earthRadius*Math.cos(lat))) + "px";
 	}
 });
 
